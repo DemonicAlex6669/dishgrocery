f17d11027736e25e4c3a2bb68e1bc8575053b7dd
alex watson <alexwatsondemon@gmail.com>
2025-02-28 13:39:07 -0800

    began

---
+++ dishgrocery.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+main():
+    url = ...
+    responce = request.get(url)
+    data = responce.json()
+
+    ingrediant = ...
+
+    for ingrediant in ingrediants:
+        print(f"{ingrediant} ok? ")
+        answer = input()
+        if answer.lower == "yes" or "y":
+            with file("grocery.txt", mode=a) as file:
+                ...
+
+
+if __name__ == "__main__":
+    main()
