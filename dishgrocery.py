2de2e85ae0f620f2a684f69928b718226deea3f3
alex watson <alexwatsondemon@gmail.com>
2025-03-01 11:31:47 -0800

    layout

--- dishgrocery.py
+++ dishgrocery.py
@@ -13,8 +13,17 @@ main():
         print(f"{ingrediant} ok? ")
         answer = input()
         if answer.lower == "yes" or "y":
-            with file("grocery.txt", mode=a) as file:
-                ...
+            have = input("do you have this ingrediant? ")
+            if have.lower == "no" or "n":
+                with file("grocery.txt", mode=a) as file:
+                file.write(f"{ingrediant} /n")
+
+    add = input("would you like to add an ingrediant? ")
+    while add.lower == "yes" or "y":
+        with file("grocery.txt", mode=a) as file:
+            more = input("new ingrediant: ")
+            file.write("{more} /n")
+            add = input("add another? ")
 
 
 if __name__ == "__main__":
